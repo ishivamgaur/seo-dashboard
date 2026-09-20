d0f3aceb4dab80965780b65ee248a3c713dfb36e
ishivamgaur <shivam24august@gmail.com>
2026-09-20 15:59:19 +0530

    feat: logo click scrolls homepage to top

--- client/src/components/home/Wordmark.jsx
+++ client/src/components/home/Wordmark.jsx
@@ -1,13 +1,26 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { BRAND } from "@/lib/site";
 
 const [firstWord, ...restWords] = BRAND.logoWordmark.split(" ");
 
 export default function Wordmark() {
+  const pathname = usePathname();
+
+  const goTop = (e) => {
+    if (pathname === "/") {
+      e.preventDefault();
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+  };
+
   return (
     <Link
       href="/"
+      onClick={goTop}
       className="text-[15px] font-black font-mono tracking-[0.14em] text-zinc-950 dark:text-white shrink-0"
     >
       {firstWord} <span className="text-teal-500 dark:text-teal-400">{restWords.join(" ")}</span>
